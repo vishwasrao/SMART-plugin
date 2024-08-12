74fe858cdf0c644ae61d3cf965ac53d0eaa0b807
Vishwasrao Salunkhe <vishwasrao.salunkhe@gmail.com>
2024-08-12 12:05:08 -0400

    Update handler.lua

--- kong/plugins/smartplugin/handler.lua
+++ kong/plugins/smartplugin/handler.lua
@@ -1,16 +1,13 @@
 local access = require "kong.plugins.smartplugin.access"
 local kong_meta = require "kong.meta"
 
-
 local SmartHandler = {
   VERSION = kong_meta.version,
   PRIORITY = 1000,
 }
 
-
 function SmartHandler:access(conf)
   access.execute(conf)
 end
-
 
 return SmartHandler
