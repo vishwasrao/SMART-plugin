d7ef84a714e91a56c5a7109750616c1f66bd4675
Vishwasrao Salunkhe <vishwasrao.salunkhe@gmail.com>
2024-08-13 18:56:49 -0400

    Update access.lua

--- kong/plugins/smartplugin/access.lua
+++ kong/plugins/smartplugin/access.lua
@@ -1,3 +1,7 @@
+local http = require "resty.http"
+local cjson = require("cjson")
+local jwt_decoder = require("kong.plugins.jwt.jwt_parser")
+
 local fmt = string.format
 local kong = kong
 local string_find = string.find
@@ -88,6 +92,27 @@ local function launch(conf)
     -- Generate random string as UUID and store it in sessionId variable
     local sessionId = random_string()
     kong.log.debug("Session ID: ", sessionId)
+
+    -- Insert data into smart_launches table
+    local smart_launches = kong.db.smart_launches
+    local data = {
+        session_id = sessionId,
+        client_id = appDetails.client_id,
+        client_secret = appDetails.client_secret,
+        app_name = appDetails.app_name,
+        app_launch_url = appDetails.app_launch_url,
+        app_redirect_url = appDetails.app_redirect_url,
+        token_endpoint_url = tokenEndpoint,
+        fhir_server_url = iss,
+        scope = appDetails.scope
+    }
+    local res, err = smart_launches:insert(data)
+    if err then
+        kong.log.err("Error inserting data into smart_launches table: ", err)
+        return kong.response.exit(500, { message = "Error inserting data into smart_launches table" })
+    end
+    kong.log.debug("Data inserted into smart_launches table: ", res)
+
     -- Redirect to authorize endpoint with query params client_id, scope, redirect_uri etc.
     local authorize_url = authorizeEndpoint ..
         "?response_type=code" ..
@@ -104,7 +129,112 @@ end
 
 local function authcallback(conf)
     kong.log.debug("SMART plugin: authcallback handler")
-    kong.log.inspect(kong.request.get_headers())
+   
+    -- Get query parameters
+    local query_params = kong.request.get_query()
+
+    -- Get code, state, error, error_description, and error_uri from query parameters
+    local code = query_params["code"]
+    local state = query_params["state"]
+    local error = query_params["error"]
+    local error_description = query_params["error_description"]
+    local error_uri = query_params["error_uri"]
+
+    -- Check if error exists
+    if error then
+        kong.log.err("Received error: ", error)
+        if error_uri then
+            kong.response.set_header("Location", error_uri)
+            kong.response.exit(302)
+        else
+            return kong.response.exit(403, { message = "Error occurred during authcallback", error = error, error_description = error_description })
+        end
+    end
+
+    -- Validate code and state are not empty
+    if not code or not state then
+        kong.log.err("Missing code or state in the request")
+        return kong.response.exit(400, { message = "Missing code or state in the request" })
+    end
+
+    -- Fetch data from smart_launches table using sessionId
+    local smart_launches = kong.db.smart_launches
+    local smart_launches_details, err = smart_launches:select({ session_id = state })
+    if err then
+        kong.log.err("Error fetching data from smart_launches table: ", err)
+        return kong.response.exit(500, { message = "Error fetching data from smart_launches table" })
+    end
+
+    -- Check if data exists
+    if not smart_launches_details then
+        kong.log.err("Data not found for sessionId: ", state)
+        return kong.response.exit(404, { message = "Data not found" })
+    end
+    --Print the smart_launches details
+    kong.log.inspect("Data fetched from smart_launches table: ", smart_launches_details)
+
+    -- Send POST request to token endpoint
+    
+    local httpc = http.new()
+    local res, err = httpc:request_uri(smart_launches_details.token_endpoint_url, {
+        method = "POST",
+        headers = {
+            ["Content-Type"] = "application/x-www-form-urlencoded",
+            ["Authorization"] = "Basic " .. ngx.encode_base64(smart_launches_details.client_id .. ":" .. smart_launches_details.client_secret)
+        },
+        body = ngx.encode_args({
+            scope = smart_launches_details.scope,
+            redirect_uri = smart_launches_details.app_redirect_url,
+            code = code,
+            grant_type = "authorization_code"
+        })
+    })
+
+    if not res then
+        kong.log.err("Error sending POST request to token endpoint: ", err)
+        return kong.response.exit(500, { message = "Error sending POST request to token endpoint" })
+        end
+
+        local access_token_response = res.body
+        kong.log.debug("Received response from token endpoint: ", access_token_response)
+
+        -- Parse the response to get access_token, token_type, expires_in, patient, refresh_token, id_token, Optionally encounter, location
+        local response = cjson.decode(access_token_response)
+        local access_token = response.access_token
+        local token_type = response.token_type
+        local expires_in = response.expires_in
+        local patient = response.patient
+        local refresh_token = response.refresh_token
+        local id_token = response.id_token
+        local encounter = response.encounter
+        local location = response.location
+
+        -- Decode id_token to get the user details, get fhirUser from it
+        local jwt_obj = jwt_decoder:new(id_token)
+        kong.log.debug("id_token: ", jwt_obj)
+        --local user_details = jwt_obj:parse()
+        --local fhirUser = user_details.fhirUser
+
+        -- Store all these details in smart_launches table using sessionId
+        local smart_launches = kong.db.smart_launches
+        local update_data = {
+        access_token = access_token,
+        token_type = token_type,
+        --expires_in = expires_in,
+        patient = patient,
+        refresh_token = refresh_token,
+        id_token = id_token,
+        encounter = encounter,
+        location = location,
+        --fhirUser = fhirUser
+        }
+        local update_res, update_err = smart_launches:update({ session_id = state }, update_data)
+        if update_err then
+        kong.log.err("Error updating data in smart_launches table: ", update_err)
+        return kong.response.exit(500, { message = "Error updating data in smart_launches table" })
+        end
+        kong.log.debug("Data updated in smart_launches table: ", update_res)
+    -- Continue with the rest of the code
 end
 
 local function invalid_method(endpoint_name, realm)
